15adaa10a805a3520ef3040e567fce4f2fbfde04
BuWH <wenhelog@gmail.com>
2018-02-26 12:37:51 +0800

    continue testing

--- pyto.py
+++ pyto.py
@@ -1,11 +1,3 @@
-import math
-def quadratic(a,b,c):
-    if (b*b-4*a*c) < 0:
-        raise TypeError('false data')
-    else:
-        t = math.sqrt(b*b-4*a*c)
-        x = (-b+t)/(2*a)
-        y = (-b-t)/(2*a)
-    return x,y
-
-print(quadratic(1,-2,1))
+d = {'city':'Beijing','sex':'male'}
+for k,v in d.items():
+    print(k,':',v)

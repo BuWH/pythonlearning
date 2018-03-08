d13acf7d1a6019c6dbb562b837f8f5a0c5f4084f
BuWH <wenhelog@gmail.com>
2018-03-08 23:02:34 +0800

    start learning crawler

---
+++ crawler.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-  
+
+#以下这段用于避免输出时出现编码错误
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+#函数主体如下
+
+import urllib.request
+import urllib.error
+
+try:
+    response = urllib.request.urlopen('http://cdshishi.net')
+    f = open('website.json','w+b')
+    f.write(response.read())
+    #print(response.info())
+except urllib.error.URLError as e:
+    if hasattr(e,'reason'):
+        print('URLerror reason:'+str(e.reason))
+    if hasattr(e,'code'):
+        print('URLerror code:'+str(e.code))
+except urllib.error.HTTPError as e:
+    if hasattr(e,'reason'):
+        print('HTTPerror reason:'+str(e.reason))
+    if hasattr(e,'code'):
+        print('HTTPerror code:'+str(e.code))
+else:
+    print('Crawler is running well.')
--- pyto.py
+++ pyto.py
@@ -1,17 +1,4 @@
 # -*- coding: utf-8 -*-
-from enum import Enum, unique
-@unique
-class Gender(Enum):
-    Male = 0
-    Female = 1
-
-class Student(object):
-    def __init__(self, name, gender):
-        self.name = name
-        self.gender = gender
-
-bart = Student('Bart', Gender.Male)
-if bart.gender == Gender.Male:
-    print('Success')
-else:
-    print('Failure')
+import re
+c = list(filter(None,re.split('^([a-zA-Z]+)(@)([a-zA-Z.]+)$','wenhe@gmail.com')))
+print(c)
